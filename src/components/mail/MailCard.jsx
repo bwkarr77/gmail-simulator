145d42afe30ddf7d72d0fb8066543d73798e1c2e
bwkarr77 <bwkarr77@gmail.com>
2020-12-13 16:58:44 -0600

    updated styling

--- src/components/mail/MailCard.jsx
+++ src/components/mail/MailCard.jsx
@@ -11,6 +11,10 @@ const MailCard = ({ message }) => {
         <div className="mail-content">
           <p className="mail-author">{message.name}</p>
           <p className="mail-title">{message.title}</p>
+          <p className="mail-message">{message.message}</p>
+        </div>
+        <div className="mail-right-side">
+          <p className="mail-time">11:58 AM</p>
         </div>
       </div>
     </div>
